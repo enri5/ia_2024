12cda69be1e6cccfe9e86dd3d719380486e6784a
Enrique <enrigau3@gmail.com>
2024-10-15 12:46:48 +0200

    a

--- tictac/agent.py
+++ tictac/agent.py
@@ -20,4 +20,19 @@ class Agent(joc.Agent):
     def actua(
         self, percepcio: entorn.Percepcio
     ) -> entorn.Accio | tuple[entorn.Accio, object]:
+        
         return joc.Accio.ESPERAR
+    
+    def minimax(estat, torn_de_max):
+        if evaluar(estat, torn_de_max) != None:
+            return estat, score
+        puntuació_fills = [minimax(estat_fill, !torn_de_max) for estat_fill in estat.genera_fills()]
+        if torn_de_max:
+            return max(puntuació_fills)
+        else:
+            return min(puntuació_fills)
+
+    def evaluar(estat, jugador):
+        if estat.meta():
+            return puntuacio(jugador)
+            
